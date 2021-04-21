200503c3b3f3a6c837a1f9f9fa2c7a52559122ea
patelaniket23 <59879546+patelaniket23@users.noreply.github.com>
2021-04-20 22:54:03 -0400

    Add files via upload

---
+++ Final_Project/AuthorizationRequirements/CustomRequireClaim.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Final_Project.AuthorizationRequirements
+{
+    public class CustomRequireClaim : IAuthorizationRequirement
+    {
+        //This is request to process th authorization
+        public CustomRequireClaim(string claimType)
+        {
+            ClaimType = claimType;
+        }
+
+        public string ClaimType { get; }
+    }
+
+    public class CustomRequireClaimHandler : AuthorizationHandler<CustomRequireClaim>
+    {
+        //This is how the authorization process is going && it handles the above request
+        protected override Task HandleRequirementAsync(
+            AuthorizationHandlerContext context,
+            CustomRequireClaim requirement)
+        {
+            var hasClaim = context.User.Claims.Any(x => x.Type == requirement.ClaimType);
+            if (hasClaim)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+
+    public static class AuthorizationPolicyBuilderExtensions
+    {
+        public static AuthorizationPolicyBuilder RequireCustomClaim(
+            this AuthorizationPolicyBuilder builder,
+            string claimType)
+        {
+            builder.AddRequirements(new CustomRequireClaim(claimType));
+            return builder;
+        }
+    }
+}
